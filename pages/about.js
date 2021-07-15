02f1df475acedad9ffe8d9cce3ef3a3a1be9de8d
Fiona Ho <fwhooo@hotmail.com>
2021-07-15 10:39:49 -0400

    First commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+const About = () => {
+    return (
+        <div className={styles.container}>
+            <h1>About Page!</h1>
+            <a href="/">Home Page</a>
+        </div>
+    );
+}
+ 
+export default About;
